aa957d67f7ae780462bda0374d3d237511e0fa0a
陈菲 <chenfei@dushu365.com>
2022-02-17 15:03:41 +0800

    Site updated: 2022-02-17 15:03:41

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -2,11 +2,11 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener("visibilitychange", function () {
   if (document.hidden) {
-    $('[rel="icon"]').attr("href", "/img/trhx2.png");
+    $('[rel="icon"]').attr("href", "/images/avatar.jpeg");
     document.title = "警告！警告！";
     clearTimeout(titleTime);
   } else {
-    $('[rel="icon"]').attr("href", "/img/trhx2.png");
+    $('[rel="icon"]').attr("href", "/images/avatar.jpeg");
     document.title = "ヾ(◍°∇°◍)欢迎回来|" + OriginTitle;
     titleTime = setTimeout(function () {
       document.title = OriginTitle;
